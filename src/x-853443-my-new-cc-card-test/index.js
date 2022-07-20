0e8498b1888284d6adb06b4c9437b59a18272e41
Alfredo reza <alfredoreza@alfredos-mbp-2.lan>
2022-07-20 17:04:55 -0500

    add clickable div and action to grab sysId

--- src/x-853443-my-new-cc-card-test/index.js
+++ src/x-853443-my-new-cc-card-test/index.js
@@ -2,9 +2,14 @@ import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 
-const view = ({properties: {text= ''}}) => {
+const view = ({properties: {text1= '', text2= '', text3= '', logo= ''}}, dispatch) => {
 	return (
-		<div>{text}</div>
+		<div on-click={() => dispatch('CARD_TESTER#CLICKED_CARD', {sysId: text1})}>
+			<img src={logo} width="50" height="50"/>
+			<div><span style={{fontWeight: 'bold'}}>sysId: </span>{text1}</div>
+			<div><span style={{fontWeight: 'bold'}}>username: </span>{text2}</div>
+			<div><span style={{fontWeight: 'bold'}}>company: </span>{text3}</div>
+		</div>
 	);
 };
 
@@ -13,6 +18,14 @@ createCustomElement('x-853443-my-new-cc-card-test', {
 	view,
 	styles,
 	properties:{
-		text: {}
+		text1: {},
+		text2: {},
+		text3: {},
+		logo: {}
+	},
+	actionHandlers:{
+		"CARD_TESTER#CLICKED_CARD": ({action}) => {
+			console.log(action.payload);
+		}
 	}
 });
--- src/x-853443-my-new-cc/index.js
+++ src/x-853443-my-new-cc/index.js
@@ -2,7 +2,7 @@ import {createCustomElement} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 
-const view = ({properties: {text= '', color= 'blue'}}) => {
+const view = ({properties: {text= '', color= ''}}) => {
 	return (
 		<div style={{color}}>{text}</div>
 	);
